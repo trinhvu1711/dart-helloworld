286c4ccdfd7a1f9b63eba9397cc50ade15154a51
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2023-09-19 13:45:44 +0700

    variables

--- bin/dart_helloworld.dart
+++ bin/dart_helloworld.dart
@@ -1,3 +1,23 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  // print('Hello world!');
+  int num1 = 2;
+  double num2 = 3.2;
+  bool istrue = true;
+  String str = "asdasd";
+
+  // type checking
+  print((num1 + num2) is int);
+  print((num1 + num2).runtimeType);
+
+  // var keyword
+  var test; //dynamic type
+  var username = "longvu"; //string type
+
+  // final vs const
+  final String fullname = 'TrinhLongVu';
+  // error
+  // fullname = 'asdasd'
+  const int age = 23;
+  // error
+  // const int num = num1 + 1;
 }
